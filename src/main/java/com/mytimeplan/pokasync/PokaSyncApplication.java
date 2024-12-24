7b9a2222f518c0be5d98dc569381690d0315ce5f
Artur Kuś <artur.kus@mytimeplan.com>
2024-12-24 11:13:49 +0100

    Bootstrap project

---
+++ src/main/java/com/mytimeplan/pokasync/PokaSyncApplication.java
@@ -0,0 +1,13 @@
+package com.mytimeplan.pokasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokaSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokaSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mytimeplan/pokasync/PokaSyncMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytimeplan.pokasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokaSyncMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
