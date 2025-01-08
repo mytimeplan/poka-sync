a76f6866e3a5cdf47933da7cb95c17773912f61e
Artur Kuś <artur.kus@mytimeplan.com>
2025-01-08 15:15:29 +0100

    35655 - Fetching users from POKA

--- src/main/java/com/mytimeplan/pokasync/controllers/PokaController.java
+++ src/main/java/com/mytimeplan/pokasync/controllers/PokaController.java
@@ -2,6 +2,7 @@ package com.mytimeplan.pokasync.controllers;
 
 import com.mytimeplan.pokasync.exceptions.DefaultException;
 import com.mytimeplan.pokasync.services.poka.SkillService;
+import com.mytimeplan.pokasync.services.poka.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,9 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PokaController {
 
     private final SkillService skillService;
+    private final UserService userService;
 
     @GetMapping("/skills")
     public ResponseEntity<?> getSkills() throws DefaultException {
         return new ResponseEntity<>(skillService.getSkills(), HttpStatus.OK);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getUsers() throws DefaultException {
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
     }
 }
