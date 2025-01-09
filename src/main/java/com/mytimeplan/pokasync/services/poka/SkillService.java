9d586c4476afd863ae81b4f75cfa015b7b8ea3b6
Artur Kuś <artur.kus@mytimeplan.com>
2025-01-09 12:32:49 +0100

    35655 - Switching all methods for fetching POKA data into one with retry mechanism

--- src/main/java/com/mytimeplan/pokasync/services/poka/SkillService.java
+++ src/main/java/com/mytimeplan/pokasync/services/poka/SkillService.java
@@ -4,12 +4,13 @@ import com.mytimeplan.pokasync.dto.mtp.MtpSkillDto;
 import com.mytimeplan.pokasync.dto.poka.PokaSkillResponseDto;
 import com.mytimeplan.pokasync.exceptions.DefaultException;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,11 +26,11 @@ public class SkillService extends PokaService<PokaSkillResponseDto> {
     }
 
     public List<MtpSkillDto> getSkills() throws DefaultException {
-        PokaSkillResponseDto skillsResponse = sendRequest(0);
+        PokaSkillResponseDto skillsResponse = sendRequest(getSkillsUrl(0));
         List<PokaSkillResponseDto.Skill> pokaSkills = new ArrayList<>(skillsResponse.getResult());
 
         for (int i = LIMIT_OBJECTS; i < skillsResponse.getCount(); i += LIMIT_OBJECTS) {
-            skillsResponse = sendRequest(i);
+            skillsResponse = sendRequest(getSkillsUrl(i));
             pokaSkills.addAll(skillsResponse.getResult());
         }
 
@@ -38,20 +39,6 @@ public class SkillService extends PokaService<PokaSkillResponseDto> {
                 .map(MtpSkillDto::new)
                 .filter(mtpSkill -> StringUtils.hasText(mtpSkill.getUnitPosition()))
                 .collect(Collectors.toList());
-    }
-
-    private PokaSkillResponseDto sendRequest(int offset) throws DefaultException {
-        String skillsUrl = getSkillsUrl(offset);
-        HttpHeaders headers = getHeaders();
-        try {
-            ResponseEntity<PokaSkillResponseDto> pokaSkillResponse = restTemplate.exchange(
-                    skillsUrl, HttpMethod.GET, new HttpEntity<>(headers), PokaSkillResponseDto.class);
-            if (!isCorrectResponse(pokaSkillResponse)) throw new DefaultException("Incorrect response");
-            return pokaSkillResponse.getBody();
-        } catch (HttpClientErrorException.Forbidden ex) {
-            log.error("Forbidden error when getting skills from POKA. Check token in settings. URL:[{}] HEADERS:[{}]", skillsUrl, GSON.toJson(headers), ex);
-            return null;
-        }
     }
 
     protected List<Long> getSkillIds() throws DefaultException {
@@ -63,7 +50,7 @@ public class SkillService extends PokaService<PokaSkillResponseDto> {
         return super.isCorrectResponse(response) && response.getBody().getCount() != 0;
     }
 
-    private String getSkillsUrl(int offset) {
-        return String.format("%s/lms/skills?limit=%s&offset=%s", pokaUrl, LIMIT_OBJECTS, offset);
+    private URI getSkillsUrl(int offset) {
+        return getUri(String.format("%s/lms/skills?limit=%s&offset=%s", pokaUrl, LIMIT_OBJECTS, offset));
     }
 }
--- src/main/java/com/mytimeplan/pokasync/services/poka/UserService.java
+++ src/main/java/com/mytimeplan/pokasync/services/poka/UserService.java
@@ -5,14 +5,11 @@ import com.mytimeplan.pokasync.dto.poka.PokaUserResponseDto;
 import com.mytimeplan.pokasync.exceptions.DefaultException;
 import io.micrometer.common.util.StringUtils;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,11 +25,11 @@ public class UserService extends PokaService<PokaUserResponseDto> {
     }
 
     public List<MtpUserDto> getUsers() throws DefaultException {
-        PokaUserResponseDto usersResponse = sendRequest(0);
+        PokaUserResponseDto usersResponse = sendRequest(getUsersUrl(0));
         List<PokaUserResponseDto.User> pokaUsers = new ArrayList<>(usersResponse.getResult());
 
         for (int i = LIMIT_OBJECTS; i < usersResponse.getCount(); i += LIMIT_OBJECTS) {
-            usersResponse = sendRequest(i);
+            usersResponse = sendRequest(getUsersUrl(i));
             pokaUsers.addAll(usersResponse.getResult());
         }
 
@@ -42,26 +39,12 @@ public class UserService extends PokaService<PokaUserResponseDto> {
                 .collect(Collectors.toList());
     }
 
-    private PokaUserResponseDto sendRequest(int offset) throws DefaultException {
-        String usersUrl = getUsersUrl(offset);
-        HttpHeaders headers = getHeaders();
-        try {
-            ResponseEntity<PokaUserResponseDto> pokaUserResponse = restTemplate.exchange(
-                    usersUrl, HttpMethod.GET, new HttpEntity<>(headers), PokaUserResponseDto.class);
-            if (!isCorrectResponse(pokaUserResponse)) throw new DefaultException("Incorrect response");
-            return pokaUserResponse.getBody();
-        } catch (HttpClientErrorException.Forbidden ex) {
-            log.error("Forbidden error when getting users from POKA. Check token in settings. URL:[{}] HEADERS:[{}]", usersUrl, GSON.toJson(headers), ex);
-            return null;
-        }
-    }
-
     @Override
     protected boolean isCorrectResponse(ResponseEntity<PokaUserResponseDto> response) {
         return super.isCorrectResponse(response) && response.getBody().getCount() != 0;
     }
 
-    private String getUsersUrl(int offset) {
-        return String.format("%s/users?optional_fields=email&limit=%s&offset=%s", pokaUrl, LIMIT_OBJECTS, offset);
+    private URI getUsersUrl(int offset) {
+        return getUri(String.format("%s/users?optional_fields=email&limit=%s&offset=%s", pokaUrl, LIMIT_OBJECTS, offset));
     }
 }
