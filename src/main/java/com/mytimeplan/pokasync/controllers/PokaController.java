34ada09b6587703f91e1ac9e453cb7046f8ec43c
Artur Kuś <artur.kus@mytimeplan.com>
2025-01-10 15:59:49 +0100

    [TO DELETE] FOR TESTS

--- src/main/java/com/mytimeplan/pokasync/controllers/PokaController.java
+++ src/main/java/com/mytimeplan/pokasync/controllers/PokaController.java
@@ -6,11 +6,18 @@ import com.mytimeplan.pokasync.services.poka.UserService;
 import com.mytimeplan.pokasync.services.poka.UserSkillsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Slf4j
 @RestController
@@ -34,6 +41,19 @@ public class PokaController {
 
     @GetMapping("/users-with-skills")
     public ResponseEntity<?> getSkillsPerUser() throws DefaultException {
-        return new ResponseEntity<>(userSkillsService.comparingUsersWithSkills(), HttpStatus.OK);
+//        return new ResponseEntity<>(userSkillsService.comparingUsersWithSkills(), HttpStatus.OK);
+        try {
+            ClassPathResource resource = new ClassPathResource("static/test.json");
+
+            String fileContent = Files.readString(Path.of(resource.getURI()));
+
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            return new ResponseEntity<>(fileContent, headers, HttpStatus.OK);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
