725e9b94b4ca9beefa3d6761b94ac8e5b905a5b1
Artur Kuś <artur.kus@mytimeplan.com>
2025-01-14 09:59:21 +0100

    35655 - Adding graylog

--- src/main/java/com/mytimeplan/pokasync/PokaSyncApplication.java
+++ src/main/java/com/mytimeplan/pokasync/PokaSyncApplication.java
@@ -1,10 +1,19 @@
 package com.mytimeplan.pokasync;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@Slf4j
 @SpringBootApplication
-public class PokaSyncApplication {
+public class PokaSyncApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PokaSyncApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PokaSyncApplication.class, args);
